b97977372b52cb1eebb4593fa8373ba37a4e41d8
Rody Davis <rody.davis.jr@gmail.com>
2020-02-10 22:00:06 -0800

    updating

--- lib/src/three/three_canvasrenderer.dart
+++ lib/src/three/three_canvasrenderer.dart
@@ -4,7 +4,7 @@ library three.canvasrenderer;
 import "package:js/js.dart";
 import "three_core.dart"
     show Camera, Color, Material, MaterialParameters, Plane, Renderer, Scene;
-import "package:func/func.dart";
+import "package:func2/func.dart";
 import "dart:html" show CanvasRenderingContext2D, CanvasElement;
 
 @anonymous
